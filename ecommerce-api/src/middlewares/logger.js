d85eeeedbcf163fa9d29f6b98113b52fede5d1ce
Andhony <andhonyrpt@gmail.com>
2025-07-29 01:36:56 -0600

    Update proyecto final

--- ecommerce-api/src/middlewares/logger.js
+++ ecommerce-api/src/middlewares/logger.js
@@ -1,5 +1,4 @@
 const logger = (req, res, next) => {
-    console.log('📌 Logger ejecutado'); // Esto se debe imprimir en cada petición
   const dateTime = new Date();
   console.log(`${dateTime.toISOString()} | ${req.method} | ${req.url}`);
   next();
