2ea6ab8bb637b4473aa2f12cff5a00277fcef234
Andhony Rodolfo Palacios Téllez <andhonyrpt@gmail.com>
2026-03-14 11:58:48 -0600

    feat: reubicar la ruta de previsualización de órdenes y mantener la validación de productos

--- ecommerce-api/src/routes/orderRoutes.js
+++ ecommerce-api/src/routes/orderRoutes.js
@@ -28,17 +28,6 @@ import {
 
 const router = express.Router();
 
-// Previsualizar orden (Cálculos de servidor)
-router.post('/orders/preview', authMiddleware, [
-  body('products').notEmpty()
-    .withMessage('Products are required')
-    .isArray({ min: 1 })
-    .withMessage("Products must be a non-empty array"),
-  bodyMongoIdValidation('products.*.productId', 'Product ID'),
-  sizeValidation('products.*.size'),
-  quantityValidation('products.*.quantity'),
-], validate, previewOrder);
-
 // Obtener todas las órdenes (admin)
 router.get('/orders', authMiddleware, isAdmin, getOrders);
 
@@ -98,5 +87,15 @@ router.delete('/orders/:id', authMiddleware, isAdmin, [
   mongoIdValidation('id', 'Order ID')
 ], validate, deleteOrder);
 
+// Previsualizar orden (Cálculos de servidor)
+router.post('/orders/preview', authMiddleware, [
+  body('products').notEmpty()
+    .withMessage('Products are required')
+    .isArray({ min: 1 })
+    .withMessage("Products must be a non-empty array"),
+  bodyMongoIdValidation('products.*.productId', 'Product ID'),
+  sizeValidation('products.*.size'),
+  quantityValidation('products.*.quantity'),
+], validate, previewOrder);
 
 export default router;
